a9537dfcddf472796927d1bcb53da90f278368a5
Frederic <eid.frederic@gmail.com>
2018-11-12 00:04:48 +0200

    20181112 - Update the Geo Data with Event Brief details

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,8 +7,9 @@ db.settings({timestampsInSnapshots: true});
 
 const EVENTS_COLLECTION_FULL_DETAILS : string = "eventsCollectionFullDetails";
 const EVENTS_COLLECTION_BRIEF_DETAILS : string = "eventsCollectionBriefDetails";
+const EVENTS_COLLECTION_FULL_DETAILS_GEO : string = "eventsCollectionFullDetailsGeo";
 
-export const onEventSubmitted = 
+export const onEventSubmitted =
 functions.firestore
     .document(EVENTS_COLLECTION_FULL_DETAILS + '/{eventId}')
     .onCreate((snap, context) => {
@@ -53,4 +54,71 @@ functions.firestore
 
       // perform desired operations ...
       return db.collection(EVENTS_COLLECTION_BRIEF_DETAILS).doc(event.eventId).set(event);
+    });
+
+    export const onEventGeoCreated =
+    functions.firestore
+    .document(EVENTS_COLLECTION_FULL_DETAILS_GEO + '/{eventId}')
+    .onCreate((snap, context) => {
+      // Get an object representing the document
+      const newValue = snap.data();
+      var fullEvent : FirebaseFirestore.DocumentData;
+
+      //create a brief Event Object Template
+      interface MyEventInterface {
+        eventId : string,
+        title   : string,
+        shortDescription : string,
+        type : string,
+        actualNumberOfAttendees : number,
+        startDateTime : Date, 
+        locationLatitude : number,
+        locationLongitude: number,
+        thumbnailUrl : string,
+        source : string,
+        approved : boolean,
+        rejected : boolean,
+        g : string,
+        l : Array<number>
+    }
+
+      var fullEventDetailsRef = db.collection(EVENTS_COLLECTION_FULL_DETAILS).doc(snap.id);
+      return fullEventDetailsRef.get()
+      .then(doc => {
+                              if (!doc.exists) {
+                                console.log('No such document!');
+                                return null;
+                              } else {
+                                console.log('Document data:', doc.data());
+                                fullEvent = doc.data();
+
+                                //this is to ensure that date is still compatible with Firestore
+                                const timestamp = fullEvent.startDateTime;
+                                const startDateTime = timestamp.toDate();
+
+      //create a brief Event Object
+      const event : MyEventInterface = {
+        eventId : doc.id,
+        title   : fullEvent.title,
+        shortDescription : fullEvent.shortDescription,
+        type : fullEvent.type,
+        actualNumberOfAttendees : fullEvent.actualNumberOfAttendees,
+        startDateTime : startDateTime, 
+        locationLatitude : fullEvent.locationLatitude,
+        locationLongitude: fullEvent.locationLongitude,
+        thumbnailUrl : fullEvent.thumbnailUrl,
+        source : fullEvent.source,
+        approved : fullEvent.approved,
+        rejected : fullEvent.rejected,
+        g: newValue.g,
+        l: newValue.l
+  }
+
+      // perform desired operations ...
+      return db.collection(EVENTS_COLLECTION_FULL_DETAILS_GEO).doc(event.eventId).set(event);                  
+                              }
+                          })
+                          .catch(err => {
+                            console.log('Error getting document', err);
+                          });
     });
